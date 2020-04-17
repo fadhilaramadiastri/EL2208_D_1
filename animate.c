e77d38634cccd0b3f05987ae6c29df0a7799f4fd
syarifatull <60220712+syarifatull@users.noreply.github.com>
2020-04-17 12:40:54 +0700

    Add files via upload

---
+++ animate.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+
+void animate(int M, int N, int grid[M][N])
+{
+    int i, n;
+    printf("Masukkan jumlah iterasi yang Anda inginkan: ");
+    scanf("%d", &n);
+    //system("clear");
+    system("cls");
+    for(i = 0; i < n; i++)
+    {
+        //system("clear");
+        system("cls");
+        nextGeneration(M, N, grid);
+        Sleep(250);
+    }
+}
+
